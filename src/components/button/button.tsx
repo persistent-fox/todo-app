5dcb8f0de1af6666c8725c54a4e82a6e1115985b
Margaret <rixten.6@gmail.com>
2024-10-16 17:55:02 +0300

    add hover for buttons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -22,6 +22,7 @@ const SButton = styled.button<SButtonProps>`
 	border-radius: 5px;
 	font-weight: 400;
 	min-width: fit-content;
+	transition: all 0.3s;
 	${props =>
 		props.$isActive &&
 		css`
@@ -33,11 +34,26 @@ const SButton = styled.button<SButtonProps>`
 			color: ${props => props.theme.colors.text.primary};
 			background-color: ${props => props.theme.colors.accent};
 			&:hover {
+				color: ${props => props.theme.colors.primary};
 				background-color: ${props => props.theme.colors.tertiary};
+			}
+		`};
+	${props =>
+		props.$variant === "default" &&
+		css`
+			&:hover {
+				color: ${props => props.theme.colors.default};
+				border-color: ${props => props.theme.colors.default};
+				svg {
+					path {
+						fill: ${props => props.theme.colors.default};
+					}
+				}
 			}
 		`};
 	&:disabled {
 		background-color: ${props => props.theme.colors.grey.silver};
+		border-color: ${props => props.theme.colors.grey.silver};
 		color: ${props => props.theme.colors.grey.dark};
 		cursor: default;
 	}
